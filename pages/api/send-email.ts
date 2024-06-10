e5cc289055e0da76493431f7868c3ff48cadd76c
IOTdatabox <whaydigital@gmail.com>
2024-06-10 01:57:32 -0700

    11

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -8,6 +8,10 @@ import { BAD_REQUEST_MSG, SERVER_ERR_MSG } from "@/config/constants";
 
 // Set the SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+const EMAIL_FROM_ADDRESS = process.env.EMAIL_FROM_ADDRESS ?? "";
+const SENDGRID_TEMPLATE_ID = process.env.SENDGRID_TEMPLATE_ID ?? "";
+
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
@@ -23,6 +27,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const answerDoc = new AnswerData({ answers });
             console.log("Constructed AnswerDoc", answerDoc);
             const result = await answerDoc.save();
+
+            const toEmail = answerDoc.answers[8].email;
+            const userName = answerDoc.answers[5].text;
+
+            const emailResponse = await sendEmailWithLink(toEmail, userName);
+
             if (!result) {
                 return res.status(500).json({ success: false, err: SERVER_ERR_MSG });
             } else {
@@ -37,3 +47,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
+
+const sendEmailWithLink = async (
+    toEmail: string,
+    userName: string
+  ) => {
+    const msg = {
+      to: toEmail, // Recipient email address
+      from: {
+        email: EMAIL_FROM_ADDRESS,
+        name: 'GH2 Benefits',
+      }, // Your verified sender address
+      templateId: SENDGRID_TEMPLATE_ID,
+      dynamicTemplateData: {
+        subject: `Here are some insights to improve your dashboard.`,
+        username: userName,
+      },
+      isMultiple: false,
+    };
+  
+    try {
+      await sgMail.send(msg);
+      console.log('❤❤❤');
+      return { success: true, message: 'Email sent successfully' };
+    } catch (error) {
+      console.error(error);
+      return { success: false, error: 'Unknown error occurred' };
+      // }
+    }
+  };
+
